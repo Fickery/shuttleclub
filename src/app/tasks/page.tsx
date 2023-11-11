39a4543b1ba52aed4bd042558f680fbe8ab9648c
Fickery <fickery901@gmail.com>
2023-11-11 20:28:59 +0800

    basic checkbox

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+import TodoContent from "@/components/TodoContent";
 import TodoList from "@/components/TodoList";
 import AddTodo from "@/components/addTodo";
-import Auth from "@/components/auth";
+import Image from "next/image";
+import { SetStateAction, useState } from "react";
 
-export default function Page() {
+export default function Home() {
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  const handleTaskClick = (task: SetStateAction<null>) => {
+    setSelectedTask(task);
+  };
+
   return (
-    <main className="flex min-h-screen bg-white text-black ">
-      <AddTodo />
-      <TodoList />
+    <main className="flex min-h-screen w-full">
+      <div className="flex h-full w-3/4 flex-col items-center gap-5 bg-white p-20 text-black">
+        <AddTodo />
+        <p className="w-full pt-5 text-start text-3xl font-black">All Task</p>
+        <TodoList onTaskClick={handleTaskClick} />
+      </div>
+      <div className="w-1/5 py-20 text-black">
+        <TodoContent selectedTask={selectedTask} />
+      </div>
     </main>
   );
 }
--- src/components/TodoContent.tsx
+++ src/components/TodoContent.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
 export default function TodoContent({ selectedTask }) {
+  const [checkboxItems, setCheckboxItems] = useState([]);
+  const [allChecked, setAllChecked] = useState(false);
+
+  const handleCheckboxChange = (index) => {
+    const updatedItems = [...checkboxItems];
+    updatedItems[index].isChecked = !updatedItems[index].isChecked;
+    setCheckboxItems(updatedItems);
+  };
+
+  const handleAddCheckbox = () => {
+    setCheckboxItems([...checkboxItems, { label: "", isChecked: false }]);
+  };
+
+  const handleRemoveCheckbox = (index) => {
+    const updatedItems = [...checkboxItems];
+    updatedItems.splice(index, 1);
+    setCheckboxItems(updatedItems);
+  };
+
+  useEffect(() => {
+    const allChecked = checkboxItems.every((item) => item.isChecked);
+    setAllChecked(allChecked);
+  }, [checkboxItems]);
+
   return (
     <div className="flex flex-col justify-between">
       <p className="w-full border-none text-xs font-black text-[blue] placeholder:text-xs">
@@ -12,9 +37,44 @@ export default function TodoContent({ selectedTask }) {
             {selectedTask.title}
           </p>
           <p className="pb-5">{selectedTask.description}</p>
+
+          {/* Checkbox section */}
+          <div className="flex flex-col gap-2">
+            {checkboxItems.map((item, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  checked={item.isChecked}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+                <input
+                  type="text"
+                  placeholder="Name checkbox"
+                  value={item.label}
+                  onChange={(e) => {
+                    const updatedItems = [...checkboxItems];
+                    updatedItems[index].label = e.target.value;
+                    setCheckboxItems(updatedItems);
+                  }}
+                />
+                <button
+                  className=" text-xs text-teal-400 hover:text-teal-500"
+                  onClick={() => handleRemoveCheckbox(index)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            ))}
+            <button onClick={handleAddCheckbox}>Add Checkbox</button>
+          </div>
         </>
       )}
-      <button className="mx-auto w-full bg-teal-500 px-4 py-3 text-center text-base font-semibold text-white hover:bg-teal-300">
+      <button
+        className={`mx-auto w-full bg-teal-500 px-4 py-3 text-center text-base font-semibold text-white ${
+          !allChecked ? "cursor-not-allowed opacity-50" : "hover:bg-teal-300"
+        }`}
+        disabled={!allChecked}
+      >
         Complete
       </button>
     </div>
